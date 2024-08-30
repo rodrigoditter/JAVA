088bd100f158494ae5bb88893c7ec86eb8f4dd5e
Rodrigo Ditter Rodrigues <96592856+rodrigoditter@users.noreply.github.com>
2024-08-30 11:36:50 -0300

    Add files via upload

---
+++ PROJ01/PROJ01/src/main/java/com/mycompany/proj01/PROJ01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proj01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rodrigo.ditter
+ */
+public class PROJ01 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Escreva seu nome: ");
+        String nome = leitor.next();
+        System.out.println("Seja bem Vindo " + nome + " ao primeiro programa JAVA!");
+        
+    }
+}
